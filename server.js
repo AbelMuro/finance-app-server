d97f51b37236f6242b9d1ae7024688615c87d5af
abelmuro <abelmuro93@gmail.com>
2024-09-13 13:45:45 -0700

    commit

--- server.js
+++ server.js
@@ -14,41 +14,20 @@ const auth0 = new AuthenticationClient({
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
 });
 
-const getManagementAccessToken = async () => {
-    try{
-        const response = await fetch(`https://${process.env.AUTH0_DOMAIN}/oauth/token`, {
-            method: 'POST',
-            headers: {
-                'Content-Type' : 'application/json'
-            },
-            body: JSON.stringify({
-                client_id: process.env.AUTH0_CLIENT_ID,
-                client_secret: process.env.AUTH0_CLIENT_SECRET,
-                audience: `https://${process.env.AUTH0_DOMAIN}/api/v2/`,
-                grant_type: 'client_credentials'
-            })
-        });
-
-        const results = await response.json();
-        return results.access_token        
-    }
-    catch(error){
-        console.log(error);
-    }
-}
-
 app.use(cookieParser());
 
 app.use(bodyParser.json());
 
 app.use(cors({
-    origin: '*',						                        //Access-Control-Allow-Origin
+    origin: 'http://localhost:3000',						                        //Access-Control-Allow-Origin
     methods: ['GET', 'POST', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],			//Access-Control-Allow-Headers
     credentials: true,
     maxAge: 3600,
     optionsSuccessStatus: 200
 }))
+
+
 
 app.post('/login', async (req, res) => {
     const {email, password} = req.body;
@@ -61,18 +40,13 @@ app.post('/login', async (req, res) => {
             realm: process.env.AUTH0_REALM
         });
         const accessToken = account.data.access_token;
-        const idToken = account.data.id_token;
 
-        res.cookie('access_token', accessToken, {
+        res.cookie('accessToken', accessToken, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
-            sameSite: 'Strict'
-          });
-        res.cookie('id_token', idToken, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
-            sameSite: 'Strict'
+            sameSite: 'None'
         });
+        
 
         res.status(200).send('Login Successfull');
     }
@@ -105,27 +79,10 @@ app.post('/register', async (req, res) => {
 //the problem here is that the cookies are returning undefined, i may need to do more research on cookies and fetch requests
 
 app.get('/profile', async (req, res) => {
-    const accessToken = req.cookies.access_token;
-    const managementAccessToken = await getManagementAccessToken();
-    const userId = req.cookies.id_token;
+    const accessToken = req.cookies.accessToken;
+    console.log(accessToken);
 
-    const management = new ManagementClient({
-        domain: `https://${process.env.AUTH0_DOMAIN}`,
-        token: managementAccessToken,
-    })
-
-    if(!accessToken)
-        return res.status(403).json({message: 'User has been logged out'});
-
-    try{
-        const profile = await management.getUser({id: userId});
-        res.status(200).json(profile);
-    }
-    catch(error){
-        console.log(error);
-        res.status(403).json({error: error});
-    }
-
+    res.status(200).json({accessToken: accessToken});
 }) 
 
 
