4d13f3773d0910617bfe76b7370d1875b2e77102
abelmuro <abelmuro93@gmail.com>
2024-10-20 17:12:08 -0700

    added one endpoint that retrieves all metadata from the users account

--- server.js
+++ server.js
@@ -130,6 +130,27 @@ app.get('/profile', async (req, res) => {
     }
 });
 
+
+app.get('/get_allData', async (req, res) => {
+    const userId = req.cookies.userId;
+
+    if(!userId){
+        res.status(401).send('user has been logged out');
+        return
+    }
+    
+    try{
+        const user = await management.users.get({id: userId});
+        const userData = user.data || {};
+        const metadata = userData.user_metadata || {};
+
+        res.status(200).json(metadata);        
+    }
+    catch(error){
+        res.status(500).send(`${error.message}`);
+    }
+})
+
 app.post('/add_budget', async (req, res) => {
     const budget = req.body;
     const userId = req.cookies.userId;
