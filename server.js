ea58d1b63434cd04406f69ee8b3ab91121b7ada8
abelmuro <abelmuro93@gmail.com>
2024-10-26 13:16:28 -0700

    commit

--- server.js
+++ server.js
@@ -543,6 +543,50 @@ app.get('/get_bills', async (req, res) => {
 
         res.status(500).send(`${message}`);
     }
+});
+
+app.post('/update_income', async (req, res) => {
+    const userId = req.cookies.userId;
+    const {income} = req.body;
+
+    if(!userId){
+        res.status(401).send('user has been logged out');
+        return;
+    }
+
+    try{
+        await management.users.update({id: userId}, {
+            user_metadata: {income: income}
+        })
+        res.status(200).send('income has been updated')
+    }
+    catch(error){
+        res.status(500).send('Internal server has occurred, please try again later');
+    }
+});
+
+app.get('/get_income', async (req, res) => {
+    const userId = req.cookies.userId;
+
+    if(!userId){
+        res.status(401).send('user has been logged out');
+        return;
+    }
+
+    try{
+        const user = await management.users.get({id: userId});
+        const userData = user.data || {};
+        const metadata = userData.user_metadata || {};
+        let income = metadata.income || 0;
+
+        res.status(200).json({income: income});
+    }
+    catch(error){
+        const message = error.message;
+        res.status(500).send(`${message}`);
+    }
+
+
 })
 
 app.get('/', (req, res) => {
