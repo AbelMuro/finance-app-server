628043a0ffe434e46a66ffc3648fb8ebdb02c327
abelmuro <abelmuro93@gmail.com>
2024-11-03 13:07:04 -0800

    commit

--- src/Routes/POST/Register.js
+++ src/Routes/POST/Register.js
@@ -19,7 +19,8 @@ router.post('/register', async (req, res) => {
         res.status(200).send('Account has been registered');
     }
     catch(error){
-        res.status(400).send(`${error.message} ${email} ${name} ${password}`);
+        const message = error.message;
+        res.status(400).send(`${message}`);
     }
 });
 
